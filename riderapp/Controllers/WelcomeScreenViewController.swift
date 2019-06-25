695dabbd1784647b0f3fc61d347e68facca4a810
jonesgreg <mrjonesgregory@gmail.com>
2019-06-25 13:27:21 -0400

    added transition to the new view controller

--- riderapp/Controllers/WelcomeScreenViewController.swift
+++ riderapp/Controllers/WelcomeScreenViewController.swift
@@ -1,32 +1,23 @@
 //
-//  ViewController.swift
+//  WelcomeScreenViewController.swift
 //  riderapp
 //
-//  Created by Gregory Jones on 6/17/19.
+//  Created by Gregory Jones on 6/25/19.
 //  Copyright © 2019 Gregory Jones. All rights reserved.
 //
 
 import UIKit
 import Lottie
-
-
-class ViewController: UIViewController {
-   
+class WelcomeScreenViewController: UIViewController {
     
-    private var revealingLoaded = false
-  
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(animationView)
-        setupLayout()
-        hideNavigationBar()
-        switchScreen()
-       
+        //setupLayout()
     }
-   
     
     let animationView: AnimationView = {
-        let animation = AnimationView(name: "Loading_Animation")
+        let animation = AnimationView(name: "car_animation")
         animation.loopMode = .loop
         animation.play()
         animation.translatesAutoresizingMaskIntoConstraints = false
@@ -40,21 +31,5 @@ class ViewController: UIViewController {
         animationView.heightAnchor.constraint(equalToConstant: 500).isActive = true
         
     }
-   
-    func switchScreen() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { // Delay the time to push to the next view controller by 7 seconds
-            let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "WelcomeScreen")
-            self.show(nextViewController!, sender: nil)
-        }
-    }
     
- 
-   func hideNavigationBar() {
-      self.navigationController?.setNavigationBarHidden(true, animated: true)
-  }
-
-    
-
-
-} // end of the class
-
+}
